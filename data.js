28e64fa9b696293ad207af0b1d3f6b9a033961e5
David Prismantas <davidprismantas@gmail.com>
2020-07-02 01:38:09 -0700

    added data.js file and index.js file

---
+++ data.js
@@ -0,0 +1,34 @@
+var people = [
+    {
+        "First":"Ted",
+        "Last":"Tandembike",
+        "age":34,
+        "job":"pilot"
+    },
+    {
+        "First":"Sarah",
+        "Last":"Smith",
+        "age":23,
+        "job":"journalist"
+    },
+    {
+        "First":"Peter",
+        "Last":"Piper",
+        "age":47,
+        "job":"writer"
+    },
+    {
+        "First":"Isabel",
+        "Last":"Indigo",
+        "age":38,
+        "job":"biologist"
+    },
+    {
+        "First":"David",
+        "Last":"Donovan",
+        "age":15,
+        "job":"student"
+    }
+]
+
+module.exports = people;
