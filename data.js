0d849b1246eed39073bbb9a9b63be881955412da
David Prismantas <davidprismantas@gmail.com>
2020-07-03 00:39:29 -0700

    Assignment 1, final commit

--- data.js
+++ data.js
@@ -1,34 +1,38 @@
 var people = [
     {
-        "First":"Ted",
-        "Last":"Tandembike",
-        "age":34,
-        "job":"pilot"
+        First:"Ted",
+        Last:"Tandembike",
+        age:34,
+        job:"pilot"
     },
     {
-        "First":"Sarah",
-        "Last":"Smith",
-        "age":23,
-        "job":"journalist"
+        First:"Sarah",
+        Last:"Smith",
+        age:23,
+        job:"journalist"
     },
     {
-        "First":"Peter",
-        "Last":"Piper",
-        "age":47,
-        "job":"writer"
+        First:"Peter",
+        Last:"Piper",
+        age:47,
+        job:"writer"
     },
     {
-        "First":"Isabel",
-        "Last":"Indigo",
-        "age":38,
-        "job":"biologist"
+        First:"Isabel",
+        Last:"Indigo",
+        age:38,
+        job:"biologist"
     },
     {
-        "First":"David",
-        "Last":"Donovan",
-        "age":15,
-        "job":"student"
+        First:"David",
+        Last:"Donovan",
+        age:15,
+        job:"student"
     }
-]
+];
 
-module.exports = people;
+exports.getAll = () => {
+    return people;
+};
+
+// module.exports = people;
--- index.js
+++ index.js
@@ -1,4 +1,81 @@
-const http = require("http");
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const data = require('./data');
 
+
+http.createServer((req,res) => {
+    const thePath = req.url.toLowerCase();
+    switch(thePath) {
+      case '/':
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Home page</h1>' + '\n' + 'Array length: ' + data.getAll().length);
+        break;
+      case '/about':
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, data) => {
+            if(err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        });  
+        break;
+      default:
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not found');
+        break;
+      }
+  }).listen(process.env.PORT || 3000, () => console.log('Server running on port 3000'));
+
+
+
+// 2
+// Old version; simple text, does not use html or css
+
+// http.createServer((req,res) => {
+//     const path = req.url;
+//     switch(path) {
+//       case '/':
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.end('<h1>Home page</h1>' + '\n' + 'Array length: ' + data.getAll().length);
+//         break;
+//       case '/about':
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.end('About page' + '\n' + '\n' + 'My name is David and this is my website!');
+//         break;
+//       default:
+//         res.writeHead(404, {'Content-Type': 'text/plain'});
+//         res.end('Not found');
+//         break;
+//       }
+//   }).listen(process.env.PORT || 3000, () => console.log('Server running on port 3000'));
+
+
+
+
+// 3
+//Does not work; the else statement causes the first condition (the '/' path) to do nothing, and shows up as "Not found" (goes to the else statement)
+
+// http.createServer((req,res) => {
+//     if(req.url === '/') {
+//         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+//             if(err) throw err;
+//             res.writeHead(200, {'Content-Type': 'text/html'});
+//             res.end(data);
+//         });  
+//     }
+
+//     if(req.url === '/about') {
+//         fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, data) => {
+//             if(err) throw err;
+//             res.writeHead(200, {'Content-Type': 'text/html'});
+//             res.end(data);
+//         });  
+//     }
+
+//     else {
+//         res.writeHead(404, {'Content-Type': 'text/plain'});
+//         res.end('Not found');
+//     }
+
+        
+//   }).listen(process.env.PORT || 3000, () => console.log('Server running on port 3000'));
